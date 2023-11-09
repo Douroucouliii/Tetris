8ff71a136055e72488c36bc6db36fca06e6cab23
Douroucouli <perin.clement@hotmail.com>
2023-11-09 18:35:23 +0100

    1.0.2

---
+++ README.txt
@@ -0,0 +1,9 @@
+Tout ce qu'on doit faire :
+
+- Modèle du tétris
+- Version texte du tétris
+- Vue Ncurses (ouch)
+- Vue SDL (ouch)
+- Contrôleurs
+- Rajouter des sons
+- Animations
--- modele.c
+++ modele.c
@@ -3,49 +3,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <rand.h>
 
-enum couleur{RIEN, CYAN, JAUNE, VIOLET, ORANGE, BLEU, ROUGE, VERT};
-typedef enum couleur couleur;
-
-typedef struct{
-    unsigned i : 6; //Coordonnée x de la cellule
-    unsigned j : 5; //Coordonnée y de la cellule
-    bool estRempli; //Boolean si la cellule est occupé par une piece
-    couleur c; //Couleur de la cellule
-}cellule;
+enum color{NOTHING, CYAN, YELLOW, PURPLE, ORANGE, BLUE, RED, GREEN};
+typedef enum color color;
 
 typedef struct{
-    cellule p[4]; //piece (tableau de 4 cellule)
+    unsigned i : 12; //Coordonnée x de la cellule
+    unsigned j : 12; //Coordonnée y de la cellule
+    bool isFull; //Boolean si la cell est occupé par une piece
+    color c; //Couleur de la cellule
+}cell;
+
+typedef struct{
+    char* nom; //Nom de la pièce
+    cell p[4]; //Piece (tableau de 4 cellules)
+    color c; //Couleur de la pièce
 }piece;
 
-const char H = 20;
-const char L = 10;
+const char HAUTEUR = 20;
+const char LARGEUR = 10;
 
-cellule** init_grille() {
-    cellule** grille = (cellule **) malloc(H * sizeof(cellule*));
-    if(!grille){
+cell** init_board() {
+    cell** board = (cell **) malloc(HAUTEUR * sizeof(cell*));
+    if(!board){
         perror("Erreur malloc()\n");
         exit(EXIT_FAILURE);
     }
-    for(int i=0; i < H; i++) {
-        grille[i] = (cellule *) malloc(L * sizeof(cellule));
-        if (!grille[i]) {
+    for(int i=0; i < HAUTEUR; i++) {
+        board[i] = (cell *) malloc(LARGEUR * sizeof(cell));
+        if (!board[i]) {
             perror("Erreur malloc()\n");
             exit(EXIT_FAILURE);
         }
-        for (int j = 0; j < L; j++) {
-            grille[i][j].i = i;
-            grille[i][j].j = j; 
-            grille[i][j].estRempli = false;
-            grille[i][j].c = RIEN;
+        for (int j = 0; j < LARGEUR; j++) {
+            board[i][j].i = i;
+            board[i][j].j = j; 
+            board[i][j].estRempli = false;
+            board[i][j].c = RIEN;
         }
     }
-    return grille;
+    return board;
 }
 
-void display_grille(cellule** c){
-    for(int i=0; i<H; i++){
-        for(int j=0; j<L; j++){
+void display_board(cell** c){
+    for(int i=0; i<HAUTEUR; i++){
+        for(int j=0; j<LARGEUR; j++){
             if(!c[i][j].estRempli) printf(" ");
             else printf("#");
         }
@@ -54,8 +57,45 @@ void display_grille(cellule** c){
     printf("\n");
 }
 
-void clear(cellule** c){
-    for(int i = 0; i < H; i++) {
+piece** init_tmp_piece(){
+    //Ici on crée les 7 pieces du jeu et on les stock dans le tableau
+    for(int i=0; i<7; i++){
+        tmpPiece[i] = (piece *) malloc(sizeof(piece));
+    }
+    tmpPiece[0]->nom = "I";
+    tmpPiece[1]->nom = "O";
+    tmpPiece[2]->nom = "T";
+    tmpPiece[3]->nom = "L";
+    tmpPiece[4]->nom = "J";
+    tmpPiece[5]->nom = "Z";
+    tmpPiece[6]->nom = "S";
+}
+
+piece* getPiece(){
+    //Ajouter la piece au tableau de pieces
+}
+
+void moveDownPiece(cell** c, piece *p){
+    //Et vérif que c'est pas tout en bas
+}
+void moveLeftPiece(cell** c, piece *p){
+    //Et vérif que c'est pas tout à gauche
+}
+void moveRightPiece(cell** c, piece *p){
+    //Et vérif que c'est pas tout à droite
+}
+
+void rotateLeft(cell** c, piece *p){
+    //Si possible
+}
+void rotateRight(cell** c, piece *p){
+    //Si possible
+}
+
+
+
+void clear(cell** c){
+    for(int i = 0; i < HAUTEUR; i++) {
         free(c[i]);
     }
     free(c);
@@ -65,11 +105,20 @@ int main(int argc, char *argv[]){
     argc--;
     argv++;
 
-    cellule** grille = init_grille();
-    display_grille(grille);
+    srand(time(NULL));
+
+    //Tableau de pointeur de piece (les 7 pieces du jeu), qui serviront pour memcpy. Evite de devoir regénérer une piece à chaque fois.
+    piece[7][] tmpPiece = init_tmp_piece();
+
+    //Tableau de pointeur de piece qui stocke toutes les pièces qui ont été dans le jeu.
+    piece** boardPiece = NULL;
+
+    //Tableau de cellule, c'est notre jeu
+    cell** board = init_board();
+    display_board(board);
 
 
-    clear(grille);
+    clear(board);
 
     return EXIT_SUCCESS;
 }
