0b380249db3a19692ced5a9d1ab0b87a625969e8
Carole-prog <72562454+Carole-prog@users.noreply.github.com>
2023-11-10 23:05:09 +0100

    Change of display + Others
    -> Nouveau plateau Tetris
    ->Couleurs attribuer avec le code ANSI du terminal
    -> Plateau ( display ) revue complétement
    -> restructuration des structures pour les prochaines fonctions ( rotate , move )
    -> le prob de free régler du coup

--- modele.c
+++ modele.c
@@ -9,6 +9,7 @@
 enum color{NOTHING, CYAN, YELLOW, PURPLE, ORANGE, BLUE, RED, GREEN};
 typedef enum color color;
 
+
 typedef struct{
     unsigned i : 12; //Coordonnée x de la cellule
     unsigned j : 12; //Coordonnée y de la cellule
@@ -17,26 +18,45 @@ typedef struct{
 }cell;
 
 typedef struct{
-    char nom; //Nom de la pièce
-    cell p[4]; //Piece (tableau de 4 cellules)
-}piece;
+    char nom;
+    int num_cells;
+    int coords[4][2];
+    color c;
+}PieceConfig;
 
-const char HAUTEUR = 20;
-const char LARGEUR = 10;
+
+typedef struct{
+    char nom; //Nom de la pièce
+    int x;
+    int y;
+}PlacedPiece;
+
+const char HEIGHT = 20;
+const char WIDTH = 10;
+
+PieceConfig pieces[7] = {
+    {'I', 4, {{0, 3}, {0, 4}, {0, 5}, {0, 6}}, RED},
+    {'O', 4, {{0, 4}, {0, 5}, {1, 4}, {1, 5}}, ORANGE},
+    {'T', 4, {{0, 3}, {0, 4}, {0, 5}, {1, 4}}, GREEN},
+    {'L', 4, {{0, 3}, {0, 4}, {0, 5}, {1, 5}}, CYAN},
+    {'J', 4, {{0, 3}, {0, 4}, {0, 5}, {1, 3}}, YELLOW},
+    {'Z', 4, {{0, 4}, {0, 5}, {1, 5}, {1, 6}}, BLUE},
+    {'S', 4, {{0, 4}, {0, 5}, {1, 3}, {1, 4}}, PURPLE}
+};
 
 cell** init_board() {
-    cell** board = (cell **) malloc(HAUTEUR * sizeof(cell*));
+    cell** board = (cell **) malloc(HEIGHT * sizeof(cell*));
     if(!board){
         perror("Erreur malloc()\n");
         exit(EXIT_FAILURE);
     }
-    for(int i=0; i < HAUTEUR; i++) {
-        board[i] = (cell *) malloc(LARGEUR * sizeof(cell));
+    for(int i=0; i < HEIGHT; i++) {
+        board[i] = (cell *) malloc(WIDTH * sizeof(cell));
         if (!board[i]) {
             perror("Erreur malloc()\n");
             exit(EXIT_FAILURE);
         }
-        for (int j = 0; j < LARGEUR; j++) {
+        for (int j = 0; j < WIDTH; j++) {
             board[i][j].i = i;
             board[i][j].j = j; 
             board[i][j].isFull = false;
@@ -46,244 +66,153 @@ cell** init_board() {
     return board;
 }
 
-void display_board(cell** c){
-    for(int i=0; i<HAUTEUR; i++){
-        for(int j=0; j<LARGEUR; j++){
-            if(!c[i][j].isFull) printf(".");
-            else printf("#");
-        }
-        printf("\n");
+void display_board(cell** c) {
+    printf("┌");
+    for (int i = 0; i < WIDTH * 2; i++) {
+        printf("─");
     }
-    printf("\n");
+    printf("┐\n");
+
+    for (int i = 0; i < HEIGHT; i++) {
+        printf("│");
+        for (int j = 0; j < WIDTH; j++) {
+            if (!c[i][j].isFull)
+                printf("  ");
+            else {
+                // Utiliser des codes ANSI de couleur pour chaque pièce
+                switch (c[i][j].c) {
+                    case NOTHING:
+                        printf("  ");
+                        break;
+                    case CYAN:
+                        printf("\033[0;36m[]\033[0m");
+                        break;
+                    case YELLOW:
+                        printf("\033[1;33m[]\033[0m");
+                        break;
+                    case PURPLE:
+                        printf("\033[0;35m[]\033[0m");
+                        break;
+                    case ORANGE:
+                        printf("\033[0;33m[]\033[0m");
+                        break;
+                    case BLUE:
+                        printf("\033[0;34m[]\033[0m");
+                        break;
+                    case RED:
+                        printf("\033[0;31m[]\033[0m");
+                        break;
+                    case GREEN:
+                        printf("\033[0;32m[]\033[0m");
+                        break;
+                    default:
+                        printf("[]");
+                }
+            }
+        }
+        printf("│\n");
+    }
+
+    printf("└");
+    for (int i = 0; i < WIDTH * 2; i++) {
+        printf("─");
+    }
+    printf("┘\n\n");
 }
 
-piece** init_tmpPiece(){
-    //Ici on crée les 7 pieces du jeu et on les stock dans le tableau
-    char noms_pieces[] = {'I', 'O', 'T', 'L', 'J', 'Z', 'S'};
-    //On alloue le tableau
-    piece** tmpPiece = (piece**) malloc(7 * sizeof(piece *));
-    if(!tmpPiece){
+
+
+PieceConfig** init_tmpPiece() {
+    PieceConfig** tmpPiece = (PieceConfig**) malloc(7 * sizeof(PieceConfig*));
+    if (!tmpPiece) {
         perror("Erreur malloc()\n");
         exit(EXIT_FAILURE);
     }
+
     for (int i = 0; i < 7; i++) {
-        tmpPiece[i] = (piece *)malloc(sizeof(piece));
+        tmpPiece[i] = (PieceConfig*) malloc(sizeof(PieceConfig));
         if (!tmpPiece[i]) {
             perror("Erreur malloc()\n");
             exit(EXIT_FAILURE);
         }
-        //On initialise à vide
-        tmpPiece[i]->nom = noms_pieces[i];
+
+        // Initialise à vide
+        tmpPiece[i]->nom = pieces[i].nom;
+        tmpPiece[i]->num_cells = pieces[i].num_cells;
+        tmpPiece[i]->c = pieces[i].c;
+
         for (int j = 0; j < 4; j++) {
-            tmpPiece[i]->p[j].i = 0;
-            tmpPiece[i]->p[j].j = 0;
-            tmpPiece[i]->p[j].isFull = false;
-            tmpPiece[i]->p[j].c = NOTHING;
+            tmpPiece[i]->coords[j][0] = 0;
+            tmpPiece[i]->coords[j][1] = 0;
         }
-        //On crée toutes les pièces
-        if(tmpPiece[i]->nom == 'I'){
-            tmpPiece[i]->p[0].i = 0;
-            tmpPiece[i]->p[0].j = 3;
-            tmpPiece[i]->p[0].isFull=true;
-            tmpPiece[i]->p[0].c=RED;
 
-            tmpPiece[i]->p[1].i = 0;
-            tmpPiece[i]->p[1].j = 4;
-            tmpPiece[i]->p[1].isFull=true;
-            tmpPiece[i]->p[1].c=RED;
-
-            tmpPiece[i]->p[2].i = 0;
-            tmpPiece[i]->p[2].j = 5;
-            tmpPiece[i]->p[2].isFull=true;
-            tmpPiece[i]->p[2].c=RED;
-
-            tmpPiece[i]->p[3].i = 0;
-            tmpPiece[i]->p[3].j = 6;
-            tmpPiece[i]->p[3].isFull=true;
-            tmpPiece[i]->p[3].c=RED;
-        }
-        else if(tmpPiece[i]->nom == 'O'){
-            //On crée la pièce
-            tmpPiece[i]->p[0].i = 0;
-            tmpPiece[i]->p[0].j = 4;
-            tmpPiece[i]->p[0].isFull=true;
-            tmpPiece[i]->p[0].c=RED;
-
-            tmpPiece[i]->p[1].i = 0;
-            tmpPiece[i]->p[1].j = 5;
-            tmpPiece[i]->p[1].isFull=true;
-            tmpPiece[i]->p[1].c=RED;
-
-            tmpPiece[i]->p[2].i = 1;
-            tmpPiece[i]->p[2].j = 4;
-            tmpPiece[i]->p[2].isFull=true;
-            tmpPiece[i]->p[2].c=RED;
-
-            tmpPiece[i]->p[3].i = 1;
-            tmpPiece[i]->p[3].j = 5;
-            tmpPiece[i]->p[3].isFull=true;
-            tmpPiece[i]->p[3].c=RED;
-        }
-        else if(tmpPiece[i]->nom == 'T'){
-            //On crée la pièce
-            tmpPiece[i]->p[0].i = 0;
-            tmpPiece[i]->p[0].j = 4;
-            tmpPiece[i]->p[0].isFull=true;
-            tmpPiece[i]->p[0].c=RED;
-
-            tmpPiece[i]->p[1].i = 0;
-            tmpPiece[i]->p[1].j = 5;
-            tmpPiece[i]->p[1].isFull=true;
-            tmpPiece[i]->p[1].c=RED;
-
-            tmpPiece[i]->p[2].i = 0;
-            tmpPiece[i]->p[2].j = 6;
-            tmpPiece[i]->p[2].isFull=true;
-            tmpPiece[i]->p[2].c=RED;
-
-            tmpPiece[i]->p[3].i = 1;
-            tmpPiece[i]->p[3].j = 5;
-            tmpPiece[i]->p[3].isFull=true;
-            tmpPiece[i]->p[3].c=RED;
-        }
-        else if(tmpPiece[i]->nom == 'L'){
-            //On crée la pièce
-            tmpPiece[i]->p[0].i = 0;
-            tmpPiece[i]->p[0].j = 4;
-            tmpPiece[i]->p[0].isFull=true;
-            tmpPiece[i]->p[0].c=RED;
-
-            tmpPiece[i]->p[1].i = 0;
-            tmpPiece[i]->p[1].j = 5;
-            tmpPiece[i]->p[1].isFull=true;
-            tmpPiece[i]->p[1].c=RED;
-
-            tmpPiece[i]->p[2].i = 0;
-            tmpPiece[i]->p[2].j = 6;
-            tmpPiece[i]->p[2].isFull=true;
-            tmpPiece[i]->p[2].c=RED;
-
-            tmpPiece[i]->p[3].i = 1;
-            tmpPiece[i]->p[3].j = 4;
-            tmpPiece[i]->p[3].isFull=true;
-            tmpPiece[i]->p[3].c=RED;
-        }
-        else if(tmpPiece[i]->nom == 'J'){
-            //On crée la pièce
-            tmpPiece[i]->p[0].i = 0;
-            tmpPiece[i]->p[0].j = 4;
-            tmpPiece[i]->p[0].isFull=true;
-            tmpPiece[i]->p[0].c=RED;
-
-            tmpPiece[i]->p[1].i = 0;
-            tmpPiece[i]->p[1].j = 5;
-            tmpPiece[i]->p[1].isFull=true;
-            tmpPiece[i]->p[1].c=RED;
-
-            tmpPiece[i]->p[2].i = 0;
-            tmpPiece[i]->p[2].j = 6;
-            tmpPiece[i]->p[2].isFull=true;
-            tmpPiece[i]->p[2].c=RED;
-
-            tmpPiece[i]->p[3].i = 1;
-            tmpPiece[i]->p[3].j = 6;
-            tmpPiece[i]->p[3].isFull=true;
-            tmpPiece[i]->p[3].c=RED;
-        }
-        else if(tmpPiece[i]->nom == 'Z'){
-            //On crée la pièce
-            tmpPiece[i]->p[0].i = 0;
-            tmpPiece[i]->p[0].j = 4;
-            tmpPiece[i]->p[0].isFull=true;
-            tmpPiece[i]->p[0].c=RED;
-
-            tmpPiece[i]->p[1].i = 0;
-            tmpPiece[i]->p[1].j = 5;
-            tmpPiece[i]->p[1].isFull=true;
-            tmpPiece[i]->p[1].c=RED;
-
-            tmpPiece[i]->p[2].i = 1;
-            tmpPiece[i]->p[2].j = 5;
-            tmpPiece[i]->p[2].isFull=true;
-            tmpPiece[i]->p[2].c=RED;
-
-            tmpPiece[i]->p[3].i = 1;
-            tmpPiece[i]->p[3].j = 6;
-            tmpPiece[i]->p[3].isFull=true;
-            tmpPiece[i]->p[3].c=RED;
-        }
-        else if(tmpPiece[i]->nom == 'S'){
-            //On crée la pièce
-            tmpPiece[i]->p[0].i = 0;
-            tmpPiece[i]->p[0].j = 5;
-            tmpPiece[i]->p[0].isFull=true;
-            tmpPiece[i]->p[0].c=RED;
-
-            tmpPiece[i]->p[1].i = 0;
-            tmpPiece[i]->p[1].j = 6;
-            tmpPiece[i]->p[1].isFull=true;
-            tmpPiece[i]->p[1].c=RED;
-
-            tmpPiece[i]->p[2].i = 1;
-            tmpPiece[i]->p[2].j = 4;
-            tmpPiece[i]->p[2].isFull=true;
-            tmpPiece[i]->p[2].c=RED;
-
-            tmpPiece[i]->p[3].i = 1;
-            tmpPiece[i]->p[3].j = 5;
-            tmpPiece[i]->p[3].isFull=true;
-            tmpPiece[i]->p[3].c=RED;
+        // On lui attributs les coordonnées de chaque piece ( avec le tableau pieces )
+        for (int j = 0; j < pieces[i].num_cells; j++) {
+            tmpPiece[i]->coords[j][0] = pieces[i].coords[j][0];
+            tmpPiece[i]->coords[j][1] = pieces[i].coords[j][1];
         }
     }
 
     return tmpPiece;
 }
 
-piece* get_piece(piece** tmpPiece){
-    //On crée une pièce qu'on a memcpy sur une piece de tmpPiece aléatoirement
-    //On retourne la pièce
+PieceConfig* get_piece(PieceConfig** tmpPiece) {
+    // On crée une pièce qu'on a memcpy sur une pièce de tmpPiece aléatoirement
+    // On retourne la pièce
     int randomIndex = rand() % 7;
-    piece* newPiece = (piece*)malloc(sizeof(piece));
+    PieceConfig* newPiece = (PieceConfig*) malloc(sizeof(PieceConfig));
     if (!newPiece) {
         perror("Erreur malloc()\n");
         exit(EXIT_FAILURE);
     }
-    memcpy(newPiece, tmpPiece[randomIndex], sizeof(piece));
+    memcpy(newPiece, tmpPiece[randomIndex], sizeof(PieceConfig));
 
     return newPiece;
 }
 
-void set_piece(piece* p, piece** boardPiece, cell** board, int *nbBoardPiece){
-    //Ajouter la piece au tableau de pieces (si c'est vide, on malloc, sinon realloc)
-    //Mettre à jour la grille du jeu (si une piece est déjà présente à un emplacement c'est qu'on a perdu);
-
-    *nbBoardPiece = *nbBoardPiece + 1;
-    if(!boardPiece){
-        boardPiece = (piece **) malloc(*nbBoardPiece * sizeof(piece *));
-    }else{
-        boardPiece = (piece **) realloc(boardPiece, *nbBoardPiece * sizeof(piece *));
+void set_piece(PieceConfig* p, PieceConfig*** boardPiece, cell** board, int* nbBoardPiece) {
+    // Ajouter la pièce au tableau de pièces (si c'est vide, on malloc, sinon realloc)
+    // Mettre à jour la grille du jeu (si une pièce est déjà présente à un emplacement, c'est qu'on a perdu);
+    (*nbBoardPiece)++;
+    if (!*boardPiece) {
+        *boardPiece = (PieceConfig**) malloc(*nbBoardPiece * sizeof(PieceConfig*));
+    } else {
+        *boardPiece = (PieceConfig**) realloc(*boardPiece, *nbBoardPiece * sizeof(PieceConfig*));
     }
-    if(!boardPiece){
+
+    if (!*boardPiece) {
         perror("Erreur malloc()/realloc()\n");
         exit(EXIT_FAILURE);
     }
 
-    boardPiece[*nbBoardPiece - 1] = p;
+    (*boardPiece)[*nbBoardPiece - 1] = p;
 
-    //Ici on met à jour la grille
-    for(int ind=0; ind<4; ind++){
-        if(board[p->p[ind].i][p->p[ind].j].isFull==true){
-            //Ca veut dire qu'on a perdu car les pieces sont tout en haut :(
+    // Mettre à jour la grille
+    for (int ind = 0; ind < p->num_cells; ind++) {
+        int coord_x = p->coords[ind][0];
+        int coord_y = p->coords[ind][1];
+
+        if (coord_x < 0 || coord_x >= HEIGHT || coord_y < 0 || coord_y >= WIDTH) {
+            // La pièce est en dehors des limites du plateau, donc on a perdu :(
             printf("\nTu as perdu la partie, dommage !\n\n");
             return;
-        }else{
-            board[p->p[ind].i][p->p[ind].j].isFull=true;
         }
-        board[p->p[ind].i][p->p[ind].j].c=p->p[0].c;
-    }
 
+        if (board[coord_x][coord_y].isFull == true) {
+            // La case est déjà occupée, donc on a perdu :(
+            printf("\nTu as perdu la partie, dommage !\n\n");
+            return;
+        } else {
+            board[coord_x][coord_y].isFull = true;
+            board[coord_x][coord_y].c = p->c;
+        }
+    }
 }
 
+
+
+
+/*
 void moveDownPiece(cell** c, piece *p){
     //Et vérif que c'est pas tout en bas
 }
@@ -300,19 +229,17 @@ void rotateLeft(cell** c, piece *p){
 void rotateRight(cell** c, piece *p){
     //Si possible
 }
-
-
-
+*/
 
 void clear_board(cell** c){
     if(!c) return;
-    for(int i = 0; i < HAUTEUR; i++) {
+    for(int i = 0; i < HEIGHT; i++) {
         free(c[i]);
     }
     free(c);
 }
 
-void clear_boardPiece(piece** p, int nbBoardPiece){
+void clear_boardPiece(PieceConfig** p, int nbBoardPiece){
     if(!p) return;
     for (int i=0; i<nbBoardPiece; i++) {
         free(p[i]);
@@ -320,7 +247,7 @@ void clear_boardPiece(piece** p, int nbBoardPiece){
     free(p);
 }
 
-void clear_tmpPiece(piece** p){
+void clear_tmpPiece(PieceConfig** p){
     if(!p) return;
     for (int i=0; i<7; i++) {
         free(p[i]);
@@ -329,33 +256,25 @@ void clear_tmpPiece(piece** p){
 }
 
 
-
-
 int main(int argc, char *argv[]){
     argc--;
     argv++;
-
+    
     srand(time(NULL));
 
     //Tableau de pointeur de piece (les 7 pieces du jeu), qui serviront pour memcpy. Evite de devoir regénérer une piece à chaque fois.
-    piece** tmpPiece = init_tmpPiece();
+    PieceConfig** tmpPiece = init_tmpPiece();
 
     //Tableau de pointeur de piece qui stocke toutes les pièces qui ont été dans le jeu.
-    piece** boardPiece = NULL;
+    PieceConfig** boardPiece = NULL;
     int nbBoardPiece=0;
 
     //Tableau de cellule, c'est notre jeu
     cell** board = init_board();
 
-
     //Pour l'instant krol je prend une piece aléatoire avec le get (memcpy etc), et je la met dans la grille avec le set :)
-    piece* piece = get_piece(tmpPiece);
-    set_piece(piece, boardPiece, board, &nbBoardPiece);
-    piece=get_piece(tmpPiece);
-    set_piece(piece, boardPiece, board, &nbBoardPiece);
-    free(piece);
-
-
+    PieceConfig* piece = get_piece(tmpPiece);
+    set_piece(piece, &boardPiece, board, &nbBoardPiece);
 
     //Et dcp quand j'affiche mon tableau, ploup
     display_board(board);
@@ -363,7 +282,6 @@ int main(int argc, char *argv[]){
     clear_board(board);
     clear_boardPiece(boardPiece, nbBoardPiece);
     clear_tmpPiece(tmpPiece);
-    //free(piece);
 
     return EXIT_SUCCESS;
 }
